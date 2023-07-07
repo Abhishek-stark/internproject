1dd85c9570cd872b7fb592f046dea12d3d81ceb3
AbhishekDeveleper <abhisheksingh55568@gmail.com>
2023-07-07 16:47:57 +0530

    use client added to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
 "use client";
 import React from "react";
-import { useState } from "react";
 import "./pagecontainer.css";
-import Image from "next/image";
-import { Roboto } from "next/font/google";
-import "./Component/Navigation";
-import Navigation from "./Component/Navigation";
-("use client");
 import Navbar from "./Component/Navbar";
 const page = () => {
   return (
