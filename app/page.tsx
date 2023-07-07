38a5c6324abc3101876d2632847ba27a495f4331
AbhishekDeveleper <abhisheksingh55568@gmail.com>
2023-07-07 16:45:14 +0530

    page data transfered to Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,114 +1,17 @@
 "use client";
 import React from "react";
-// import { useState } from "react";
+import { useState } from "react";
 import "./pagecontainer.css";
 import Image from "next/image";
 import { Roboto } from "next/font/google";
 import "./Component/Navigation";
 import Navigation from "./Component/Navigation";
-import Sidebar from "./Component/Sidebar";
-import Maincontent from "./Component/Maincontent";
-const roboto = Roboto({
-  weight: "700",
-  subsets: ["latin"],
-});
+("use client");
+import Navbar from "./Component/Navbar";
 const page = () => {
-  // const [classprop, seteclassprop] = useState(true);
-
-  let propsvaulue = true;
-  let classprop = propsvaulue;
-  const sendprops = () => {
-    classprop = !propsvaulue;
-  };
   return (
     <div className="pagecontainer">
-      <div className="container">
-        <div className="navbar">
-          <ul className="ul_container">
-            <li className="ul_li1">
-              <Image
-                src="./Button.svg"
-                alt="button"
-                className="vercelLogo"
-                width={50}
-                height={50}
-                priority
-                onClick={sendprops}
-              />
-            </li>
-
-            <li className="ul_li2 lic">Constructor</li>
-            <li className="ul_li3">Dashboard</li>
-            <li className="ul_li3">About Us</li>
-            <li className="ul_li3">News</li>
-            <li className="ul_li3">user Policy</li>
-            <li className="ul_li3">Contacts</li>
-            <li className="ul_li3">...</li>
-            <li className="ul_li9">
-              <Image
-                src="./searchicon.svg"
-                alt="button"
-                className="navsearchicon"
-                width={18}
-                height={18}
-                priority
-              />
-              <form action="">
-                <input
-                  type="search"
-                  placeholder="Search Products,Orders and clients"
-                />
-              </form>
-              <Image
-                src="./gtarrow.svg"
-                alt="button"
-                className="gtarrowicon"
-                width={7}
-                height={12}
-                priority
-              />
-            </li>
-            <li>
-              <Image
-                src="./account_circle.svg"
-                alt="button"
-                className="vercelLogo"
-                width={50}
-                height={50}
-                priority
-              />
-            </li>
-            <li className="ul_li3">Clayton Santos</li>
-            <li className="ul_li11">
-              {" "}
-              <Image
-                src="./alarm.svg"
-                alt="button"
-                className="vercelLogo"
-                width={50}
-                height={50}
-                priority
-              />
-            </li>
-            <li className="ul_li12">
-              {" "}
-              <Image
-                src="./cross.svg"
-                alt="button"
-                className="vercelLogo"
-                width={50}
-                height={50}
-                priority
-              />
-            </li>
-          </ul>
-        </div>
-        <div className={`${classprop ? "mainsection" : "changedmainsection"}`}>
-          <Navigation classpasses={classprop} />
-          <Sidebar />
-          <Maincontent />
-        </div>
-      </div>
+      <Navbar />
     </div>
   );
 };
